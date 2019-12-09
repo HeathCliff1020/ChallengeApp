d17a0225abdbc32c926f5dad0e0051f00008e76e
HeathCliff <heathcliff1020@gmail.com>
2019-12-09 09:10:35 -0800

    Project Folder Creation

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Challenge App</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+</head>
+
+<body>
+
+
+</body>
+
+</html>
